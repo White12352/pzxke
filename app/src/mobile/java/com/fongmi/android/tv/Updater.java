b18dc1f7caf7489d530c506c6c595f5d7bb48feb
White12352 <86940955+White12352@users.noreply.github.com>
2024-02-01 11:46:09 +0800

    Update Updater.java

--- app/src/mobile/java/com/fongmi/android/tv/Updater.java
+++ app/src/mobile/java/com/fongmi/android/tv/Updater.java
@@ -70,31 +70,31 @@ public class Updater implements Download.Callback {
     }
 
     public void start(Activity activity) {
-        App.execute(() -> doInBackground(activity));
+        //App.execute(() -> doInBackground(activity));
     }
 
     private boolean need(int code, String name) {
-        return Setting.getUpdate() && (dev ? !name.equals(BuildConfig.VERSION_NAME) && code >= BuildConfig.VERSION_CODE : code > BuildConfig.VERSION_CODE);
+        //return Setting.getUpdate() && (dev ? !name.equals(BuildConfig.VERSION_NAME) && code >= BuildConfig.VERSION_CODE : code > BuildConfig.VERSION_CODE);
     }
 
     private void doInBackground(Activity activity) {
-        try {
-            JSONObject object = new JSONObject(OkHttp.string(getJson()));
-            String name = object.optString("name");
-            String desc = object.optString("desc");
-            int code = object.optInt("code");
-            if (need(code, name)) App.post(() -> show(activity, name, desc));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //try {
+            //JSONObject object = new JSONObject(OkHttp.string(getJson()));
+            //String name = object.optString("name");
+            //String desc = object.optString("desc");
+            //int code = object.optInt("code");
+            //if (need(code, name)) App.post(() -> show(activity, name, desc));
+        //} catch (Exception e) {
+            //e.printStackTrace();
+        //}
     }
 
     private void show(Activity activity, String version, String desc) {
-        binding = DialogUpdateBinding.inflate(LayoutInflater.from(activity));
-        check().create(activity, ResUtil.getString(R.string.update_version, version)).show();
-        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(this::confirm);
-        dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setOnClickListener(this::cancel);
-        binding.desc.setText(desc);
+        //binding = DialogUpdateBinding.inflate(LayoutInflater.from(activity));
+        //check().create(activity, ResUtil.getString(R.string.update_version, version)).show();
+        //dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(this::confirm);
+        //dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setOnClickListener(this::cancel);
+        //binding.desc.setText(desc);
     }
 
     private AlertDialog create(Activity activity, String title) {
